82f0d03900988c2ad090e5fe4cdb708c33b5f507
ilhamgepe <gepedevelop@gmail.com>
2024-12-25 19:03:00 +0700

    adding more case and update package

--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/ilhamgepe/validationMessageHelper
 
-go 1.22.1
+go 1.23.4
 
-require github.com/go-playground/validator/v10 v10.19.0
+require github.com/go-playground/validator/v10 v10.23.0
 
 require (
-	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
--- validationMessageHelper.go
+++ validationMessageHelper.go
@@ -7,14 +7,12 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-
-
-func GenerateMessage(err error)map[string]string {
-	var ve validator.ValidationErrors
-	if errors.As(err, &ve){
-		messages := make(map[string]string, len(ve))
-		for _, v := range ve {
-			switch v.Tag(){
+func GenerateMessage(err error) map[string]string {
+	var vErr validator.ValidationErrors
+	if errors.As(err, &vErr) {
+		messages := make(map[string]string, len(vErr))
+		for _, v := range vErr {
+			switch v.Tag() {
 			case "email":
 				messages[v.Field()] = fmt.Sprintf("%s is not valid email", v.Value())
 			case "required":
@@ -23,6 +21,36 @@ func GenerateMessage(err error)map[string]string {
 				messages[v.Field()] = fmt.Sprintf("%s must be at least %s characters", v.Field(), v.Param())
 			case "max":
 				messages[v.Field()] = fmt.Sprintf("%s must be at most %s characters", v.Field(), v.Param())
+			case "len":
+				messages[v.Field()] = fmt.Sprintf("%s must be exactly %s characters", v.Field(), v.Param())
+			case "uuid":
+				messages[v.Field()] = fmt.Sprintf("%s is not a valid UUID", v.Field())
+			case "alpha":
+				messages[v.Field()] = fmt.Sprintf("%s must only contain alphabetic characters", v.Field())
+			case "alpha_dash":
+				messages[v.Field()] = fmt.Sprintf("%s must only contain alphabetic characters, dashes, and underscores", v.Field())
+			case "alpha_num":
+				messages[v.Field()] = fmt.Sprintf("%s must only contain alphabetic characters and numbers", v.Field())
+			case "numeric":
+				messages[v.Field()] = fmt.Sprintf("%s must be a numeric value", v.Field())
+			case "gt":
+				messages[v.Field()] = fmt.Sprintf("%s must be greater than %s", v.Field(), v.Param())
+			case "gte":
+				messages[v.Field()] = fmt.Sprintf("%s must be greater than or equal to %s", v.Field(), v.Param())
+			case "lt":
+				messages[v.Field()] = fmt.Sprintf("%s must be less than %s", v.Field(), v.Param())
+			case "lte":
+				messages[v.Field()] = fmt.Sprintf("%s must be less than or equal to %s", v.Field(), v.Param())
+			case "url":
+				messages[v.Field()] = fmt.Sprintf("%s is not a valid URL", v.Field())
+			case "hex":
+				messages[v.Field()] = fmt.Sprintf("%s must be a valid hexadecimal string", v.Field())
+			case "date":
+				messages[v.Field()] = fmt.Sprintf("%s is not a valid date, e.g: 2006-01-02", v.Field())
+			case "timezone":
+				messages[v.Field()] = fmt.Sprintf("%s is not a valid timezone e.g: UTC,+08:00,Asia,Jakarta,America,New_York", v.Field())
+			case "ip":
+				messages[v.Field()] = fmt.Sprintf("%s is not a valid IP address", v.Field())
 			}
 		}
 		return messages
